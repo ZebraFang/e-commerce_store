c6317812b6766659a35e33e0014eba57693e8b58
Greg Mansell <spockspeek@gmail.com>
2025-01-20 08:14:13 +0000

    Added Header component with Nav functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+      <h1>App</h1>
+      </Container>
+    </main>
+    
+    </>
+    
   );
 }
 
